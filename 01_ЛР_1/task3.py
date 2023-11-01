10152db7a2d396309de489cb9fbcbbe9f80ac3ee
ednovikof <149623455+ednovikof@users.noreply.github.com>
2023-11-01 23:00:30 +0300

    Add files via upload

---
+++ 01_ЛР_1/task3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+a = len(list_players)
+b = list_players[0:3]
+c = list_players[3:6]
+
+print(b)
+print(c)
