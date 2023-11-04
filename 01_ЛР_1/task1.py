3468d09cf7fc840fe31843edf58001b710915ed7
ednovikof <149623455+ednovikof@users.noreply.github.com>
2023-11-04 13:01:59 +0300

    Update task1 4.11.py

--- 01_ЛР_1/task1.py
+++ 01_ЛР_1/task1.py
@@ -1,7 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers[4] = 0
+b = input()
+b = int(b)
+numbers[b] = 0
 a = round(sum(numbers) / (len(numbers)), 2)
-numbers[4] = a
+numbers[b] = a
 
 print("Измененный список:", numbers)
